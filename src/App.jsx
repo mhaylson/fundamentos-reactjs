b7535059eb6bda4ca1e5a24fa26f22579bc6fe39
Mhaylson Whandson <mhaylson@m2smart.com.br>
2024-07-11 19:34:14 -0400

    Application structure
    Created the structure
    
    Signed-off-by: Mhaylson Whandson <mhaylson@m2smart.com.br>

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Post } from "./components/Post"
+import { Header } from "./components/Header"
+
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from "./components/Sidebar";
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+
+        <Sidebar />
+        <main>
+          <Post />
+          <Post />
+
+        </main>
+      </div>
+
+
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src='https://github.com/mhaylson.png' alt='' />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Mhaylson Whandson</strong>
+              <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atras</time>
+            </div>
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!!!! </p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
