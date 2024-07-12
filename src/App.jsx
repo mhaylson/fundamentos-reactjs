3acab02675355c57dae6e814d90684548de77475
Mhaylson Whandson <mhaylson@m2smart.com.br>
2024-07-11 20:25:57 -0400

    Post properties
    Created properties to display in posts
    
    Signed-off-by: Mhaylson Whandson <mhaylson@m2smart.com.br>

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,38 @@ import styles from './App.module.css';
 import './global.css';
 import { Sidebar } from "./components/Sidebar";
 
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/mhaylson.png',
+      name: 'Mhaylson Whandson',
+      role: 'CEO @ M2Smart'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-07-11 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/mhayk.png',
+      name: 'Mhayk Whandson',
+      role: 'CEO @ M2Smart'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-07-11 21:00:00')
+  }
+]
+
 function App() {
 
   return (
@@ -16,9 +48,18 @@ function App() {
 
         <Sidebar />
         <main>
-          <Post />
-          <Post />
-
+          {
+            posts.map(post => {
+              return (
+                <Post
+                  key={post.id}
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })
+          }
         </main>
       </div>
 
